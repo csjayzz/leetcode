e502c5e040afda91ccf478a07446482d3b9069f6
jayesh <jayeshj0304@gmail.com>
2026-02-01 01:41:22 +0530

    added sol

--- 3516. Find Closest Person.java
+++ 3516. Find Closest Person.java
@@ -26,3 +26,12 @@ Person 1 is at position 2 and can reach Person 3 (at position 4) in 2 steps.
 Person 2 is at position 7 and can reach Person 3 in 3 steps.
 Since Person 1 reaches Person 3 first, the output is 1. */
 
+class Solution {
+    public int findClosest(int x, int y, int z) {
+        if(Math.abs(z-x) > Math.abs(z-y)){
+            return 2;
+        }
+        if(Math.abs(z-x) < Math.abs(z-y))return 1;
+        return 0;
+    }
+}
