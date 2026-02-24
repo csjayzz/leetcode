5b9e6544648a980a664df9b537eb560604f80876
jayesh <jayeshj0304@gmail.com>
2026-02-24 18:09:54 +0530

    fix

--- binary tree/1022. Sum of Root To Leaf Binary Numbers.java
+++ binary tree/1022. Sum of Root To Leaf Binary Numbers.java
@@ -42,21 +42,21 @@
  * Total = 0
  */
 
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
+ //Definition for a binary tree node.
+  class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+ 
 class Solution {
     private int solve(TreeNode root, int val) {
         if (root == null) return 0;
