c8fd6295ce9feb359b24dee0a140cd41d77d1f34
jayesh <jayeshj0304@gmail.com>
2026-05-21 00:13:14 +0530

    sol

--- Array/2657. Find the Prefix Common Array of Two Arrays.java
+++ Array/2657. Find the Prefix Common Array of Two Arrays.java
@@ -50,3 +50,28 @@
  * - At each prefix, count intersection size.
  * - Constraints allow a simple O(n^2) solution.
  */
+
+
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] res = new int[n];
+        boolean[] seenA = new boolean[n + 1];
+        boolean[] seenB = new boolean[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            seenA[A[i]] = true;
+            seenB[B[i]] = true;
+
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                if (seenA[j] && seenB[j]) cnt++;
+            }
+
+            res[i] = cnt;
+        }
+
+        return res;
+    }
+}
+
